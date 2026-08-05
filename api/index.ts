ec0f83a7986821835e64c5957fae4345b271b6e8
shahidbcsm <khanshahid33200@gmail.com>
2026-08-06 01:24:11 +0530

    day foundation

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { adminDb } from './firebase-admin';
+import { adminDb } from './firebase-admin.js';
 
 const app = express();
 app.use(cors({ origin: true }));
